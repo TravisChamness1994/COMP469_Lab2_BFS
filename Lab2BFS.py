3f0ee43cc32c0907c5ea92cc8e51a0964d80db44
Travis Chamness <travisdanfordc@yahoo.com>
2021-09-03 16:10:11 -0700

    Coordinate assignment corrected

--- Lab2BFS.py
+++ Lab2BFS.py
@@ -14,10 +14,20 @@
 #
 # readFile("Map2.txt")
 
-def successorFunc(location, fringe):
-    print("fill me in")
+# Cartesian coordinate X and Y (X, Y) accessed from array
+X = 0
+Y = 1
 
+def successorFunc(location, fringe, bfsMap):
+    #check up - Negative Y direction: should never be out of bounds of the map, as long as the map is configured correctly
+    print(bfsMap[location[X]][location[Y]])
+    if bfsMap[location[X]][(location[Y] - 1)] == 1:
+        print("found the wall!")
+    #check down
 
+#Note about row and col: row is the Y coordinate, and col is our X. It looks funny flipping order
+#   going from accessor use in bfsMap[row][col] to cartesian assignment with (col, row), but it is correct
+#   Feel free to double check, Juan.
 def positionInitialization(bfsmap):
     start = [0,0]
     goal = [0,0]
@@ -25,20 +35,26 @@ def positionInitialization(bfsmap):
     for row in range(len(bfsmap)):
         for col in range(len(bfsmap[row])):
             if bfsmap[row][col] == 'R':
-                start = (row,col)
+                start = (col,row)
             if bfsmap[row][col] == 'D':
-                goal = (row,col)
+                goal = (col,row)
     return start, goal
+
+
+def buildSolutions(start, goal, fringe, pathCount, paths):
+    goalFound = False
+    # while(not goalFound and fringe):
+        # for path in range(pathCount):
 
 
 def bfsMazeSolution(bfsmap):
     start, goal = positionInitialization(bfsmap)
-    print("start, goal: {}, {}".format(start, goal))
+    # print("start, goal: {}, {}".format(start, goal))
     pathCount = 1
     fringe = [[start]]
-    print("fringe: {}".format(fringe))
-    goalfound = False
+    # print("fringe: {}".format(fringe))
     paths = [[]]
+    successorFunc(start, fringe, bfsmap)
 
 
 
