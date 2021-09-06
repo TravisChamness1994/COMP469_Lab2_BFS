7a1256ae00aa09c56b4c61f5e1c117916e972e55
Travis Chamness <travisdanfordc@yahoo.com>
2021-09-06 08:15:57 -0700

    Some commentary added, in a rush to leave the house rnrnrnrn

--- Lab2BFS.py
+++ Lab2BFS.py
@@ -2,17 +2,24 @@ COL = 1
 ROW = 0
 # COORD_MANIPULATOR_INIT = 1
 
-#Incomplete Function
 #Determines successor movements for the agent
 def successorFunc(location, fringe, bfsMap, paths, path):
+    #Branchlist is the list of possible branches or moves that are produced in the successor function
+    # based off of the current location
     branchList = []
     #check up
+    #Checking direction code pieces operate by first getting a deep copy of the original location.
+    # by subtracting or adding to the column or row, it manipulates the section of the map matrix being looked at.
     print("Location: ", location)
     copyLocation = location.copy()
+    #If the immediate dierection is not a wall
     if bfsMap[copyLocation[ROW] - 1][(copyLocation[COL])] != 1:
         copyLocation[ROW] = copyLocation[ROW] - 1
+        #continue to step in the direction until a wall is contacted
         while bfsMap[copyLocation[ROW] - 1][copyLocation[COL]] != 1:
             copyLocation[ROW] = copyLocation[ROW] - 1
+        #Once we find a wall, if the location at that point is not already visisted, add it to the branch list
+        # NOTE: Branch list is the possible successor moves
         if [copyLocation[ROW], copyLocation[COL]] not in paths[path]:
             branchList.append([copyLocation[ROW], copyLocation[COL]])
     #check down
@@ -39,7 +46,7 @@ def successorFunc(location, fringe, bfsMap, paths, path):
             copyLocation[COL] = copyLocation[COL] - 1
         if [copyLocation[ROW], copyLocation[COL]] not in paths[path]:
             branchList.append([copyLocation[ROW], copyLocation[COL]])
-    #add new branch list to fringe
+    #add new branch list to fringe. If it is empty, it will not effect the fringe, otherwise new possible moves are added
     if branchList:
         fringe.append(branchList)
     #return new fringe
@@ -59,10 +66,17 @@ def positionInitialization(bfsmap):
     return start, goal
 
 
+#Build Solution function
+# Looping until either the goal is found or the fringe is empty,
+# iterate over the fringe for the existing amount of paths.
+# Over each path, a set of possible moves exist with a minimum of 1.
+# each move will be treated as a new current location
+# As a new current location, the successor function will be run on that location, and the fringe will be updated.
+# Once done, we add the location to the associated path (this is where the error probably occurs. Need a test where a path closes)
+# Then test the location against the goal.
 def buildSolutions(start, goal, fringe, pathCount, paths, bfsMap):
     goalFound = False
     successfulPath = []
-    # print("FRINGE: ", fringe)
     iter = 1
     while(not goalFound and fringe):
         print("ITERATION ", iter)
